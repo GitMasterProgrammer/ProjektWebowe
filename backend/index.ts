febc4e7bc614c11c811c5a405a611899923b0efc
Piotr-may-zsk <piotr.may@uczen.zsk.poznan.pl>
2024-03-29 18:17:19 +0100

    Created projects

---
+++ backend/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app: Express = express();
+const port = 3000;
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    next();
+});
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+})
